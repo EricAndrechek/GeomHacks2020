1ce85ddbe8d197870b4fa94328c018e594ff8117
Eric Andrechek <ericandrechek@Erics-MacBook-Pro.local>
2020-06-13 21:13:14 -0400

    We cant figure out image depth

--- backend/measure.py
+++ backend/measure.py
@@ -3,10 +3,11 @@ import cv2
 import subprocess
 import json
 import math
+import imutils
 # import objectifyer
     
 class measure():
-    def __init__(self, qr, img, focal_length):
+    def __init__(self, qr, img, focal_length, sf):
         self.image = img
         self.txt = qr.data.decode()
         if self.txt.split(':')[0] == 'PaccuratePad':
@@ -29,13 +30,16 @@ class measure():
                     self.draw_circle((0, 255, 0))
             except IndexError:
                 self.draw_circle((0, 255, 0))
-            self.depth = self.depth_finder(self.image_width, img.shape[1], focal_length)
+            j = width/height
+            self.depth = self.depth_finder(self.image_width, self.real_width, j, 1, sf, width, focal_length)
         else:
             valid = False
 
-    def depth_finder(self, object_width, image_width, focal_length):
-        print(object_width, image_width, focal_length)
-        return (focal_length * object_width) / image_width
+    def depth_finder(self, object_width, real_width, j, k, scale_factor, image_width, focal_length):
+        print(object_width, real_width, j, k, scale_factor, image_width, focal_length)
+        real_image_width = j * math.sqrt( 43.27 / scale_factor ) / math.sqrt( math.pow(j, 2) * math.pow(k, 2) )
+        object_distance = focal_length * real_width / (object_width / real_image_width * image_width)
+        return object_distance
     
     def get_corners(self, poly):
         self.corners = poly
@@ -57,20 +61,26 @@ def get_focal_length(path):
     s.decode('utf-8').rstrip('\r\n')
     md = json.loads(s)
     fl = None
+    sf = None
     for key in md[0]:
         if 'focallength' in key.lower():
             fl = md[0][key]
-    if fl is not None:
+        if 'scalefactor' in key.lower():
+            sf = float(md[0][key])
+    if fl is not None and sf is not None:
         fl = float(fl.split()[0]) # convert mm to inches
         fl = fl / 25.4
-    return fl
+    return fl, sf
 
 def runner(img_path):
     img = cv2.imread('img-resources/{}'.format(img_path))
-    fl = get_focal_length(img_path)
+    if img.shape[1] < img.shape[0]:
+        img = imutils.rotate_bound(img, 90)
+        print('rotated')
+    fl, sf = get_focal_length(img_path)
     qr = decode(img)
     for q in qr:
-        m = measure(q, img, fl)
+        m = measure(q, img, fl, sf)
         if m.valid:
             img = m.image
             print(m.depth)
